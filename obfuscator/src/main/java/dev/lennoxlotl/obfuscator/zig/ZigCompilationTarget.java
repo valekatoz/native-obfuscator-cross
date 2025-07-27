d6427f2ad4e9ef164eea0d16530fcda7be05719c
valentino <valentino06.catozzi@gmail.com>
2025-07-27 17:06:55 +0200

    Add aarch64 Zig JNI headers and update targets
    Added JNI and related header files for aarch64 on both Linux and Windows under zig resources. Updated ZigCompilationTarget enum and config.toml to support aarch64 targets for both platforms, enabling cross-compilation and native support for ARM64 architectures. Note: i do not know if this works or not.

--- obfuscator/src/main/java/dev/lennoxlotl/obfuscator/zig/ZigCompilationTarget.java
+++ obfuscator/src/main/java/dev/lennoxlotl/obfuscator/zig/ZigCompilationTarget.java
@@ -9,10 +9,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ZigCompilationTarget {
-    WINDOWS_X86("x86_64-windows-gnu", "x64-windows.dll", "win/x86", "x64-windows"),
-    LINUX_X86("x86_64-linux-gnu", "x64-linux.so", "linux/x86", "x64-linux"),
-    MACOS_X86("x86_64-macos", "x64-macos.dylib", "macos/x64", "x64-macos"),
-    MACOS_AARCH64("aarch64-macos", "aarch64-macos.dylib", "macos/aarch64", "arm64-macos");
+    WINDOWS_X86_64("x86_64-windows-gnu", "x64-windows.dll", "win/x86", "x64-windows"),
+    WINDOWS_AARCH64("aarch64-windows-gnu", "aarch64-windows.dll", "win/aarch64", "aarch64-windows"),
+    LINUX_X86_64("x86_64-linux-gnu", "x64-linux.so", "linux/x86", "x64-linux"),
+    LINUX_AARCH64("aarch64-linux-gnu", "aarch64-linux.so", "linux/aarch64", "aarch64-linux"),
+    MACOS_X86_64("x86_64-macos", "x64-macos.dylib", "macos/x64", "x64-macos"),
+    MACOS_AARCH64("aarch64-macos", "aarch64-macos.dylib", "macos/aarch64", "aarch64-macos");
 
     private final String compileTarget;
     private final String libraryName;
